046817dd239cbbe8ac05e3d735fba6e676ed17e8
leva <leonid.lukyanov@persgroep.net>
2024-11-08 12:50:51 +0200

    Add GTM

--- simple-theme/header.php
+++ simple-theme/header.php
@@ -1,10 +1,18 @@
 <!doctype html>
 <html <?php language_attributes(); ?> >
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NDBTZM7Q');</script>
+    <!-- End Google Tag Manager -->
+
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <title><?php wpa_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<?php /* favicon */ ?>
+    <?php /* favicon */ ?>
     <link rel="apple-touch-icon" sizes="60x60" href="<?php echo theme(); ?>/img/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="<?php echo theme(); ?>/img/favicon/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="<?php echo theme(); ?>/img/favicon/favicon-16x16.png" sizes="16x16">
@@ -17,6 +25,10 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NDBTZM7Q"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php
 $logo = get_field('logo', 'options');
 $button = get_field('button', 'options');
